3a46b13e8d0c8af4bf8a82503c54cdf230594b35
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-03-28 09:46:44 +0500

    configured docker-compose, postgres and env for project

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
-const queryClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db");
+const queryClient = postgres(process.env.DATABASE_URL!);
 const db = drizzle(queryClient);
 export { db };
